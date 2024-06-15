e77c6783fe1ae1618fb1f18c6771c7f662692e28
Vishnu Kurnala <93570460+VK2004@users.noreply.github.com>
2024-06-15 09:03:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,14 +43,14 @@ if st.button('Predict Probability'):
     runs_left = target-score
     balls_left = 120 - overs*6
     wickets = 10-wickets
+    crr = score / overs if overs > 0 else 0
+    rrr = (runs_left * 6) / balls_left if balls_left > 0 else 0
     
     if (target == 0 and score == 0 and wickets == 0 and overs == 0 and batting_team != bowling_team):
         st.header('Winning Probability')
         st.header(f"{batting_team}  : 50 %")
         st.header(f"{bowling_team}  : 50 %")
     else:
-        crr = score/overs
-        rrr = runs_left*6/balls_left
         df = pd.DataFrame({'batting_team': [batting_team], 'bowling_team': [bowling_team], 'city': [selected_city], 'runs_left': [runs_left], 'balls_left': [balls_left], 'wickets': [remaining_wickets], 'total_runs_x': [target], 'crr': [crr], 'rrr': [rrr]})
         result = pipe.predict_proba(df)
         r_1 = round(result[0][0] * 100)
